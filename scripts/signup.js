26764c211175107bc3de9ded74132173a630afd9
Sean Hong <iamseanhong@gmail.com>
2019-03-24 23:53:21 -0600

    Able to create user with email and password using FE

--- scripts/signup.js
+++ scripts/signup.js
@@ -1,9 +1,20 @@
 const signupForm = document.querySelector('#signup-form');
 
-
+// const firebase = require('firebase');
+// firebase.auth().createUserWithEmailAndPassword;
 
 signupForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  
-  
+	e.preventDefault();
+
+	const email = signupForm['signup-email'].value;
+	const password = signupForm['signup-password'].value;
+	// auth.createUserWithEmailAndPassword()
+
+	auth.createUserWithEmailAndPassword(email, password).then((cred) => {
+		console.log('cred: ', cred.user);
+
+		const modal = document.querySelector('#modal-signup');
+		M.Modal.getInstance(modal).close();
+		signupForm.reset();
+	});
 });
