1f36ddaf70dc057b69ffec493d5bbe38c4ec6206
Sean Hong <iamseanhong@gmail.com>
2019-03-24 23:33:23 -0600

    Enable modal and collapsible item

---
+++ scripts/auth.js
@@ -0,0 +1,5 @@
+const signupForm = document.querySelector('#signup-form');
+
+signupForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+});
---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+// setup materalize components
+document.addEventListener('DOMContentLoaded', function() {
+
+  const modals = document.querySelectorAll('.modal');
+  M.Modal.init(modals);
+
+  const items = document.querySelectorAll('.collapsible');
+  M.Collapsible.init(items);
+})
