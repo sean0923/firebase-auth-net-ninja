263f164a96ca78327f3d8ad7ef0530b8e3760f29
Sean Hong <iamseanhong@gmail.com>
2019-03-25 13:22:11 -0600

    Render guides

---
+++ scripts/getFirestoreData.js
@@ -0,0 +1,29 @@
+const renderGuides = (docs) => {
+	const guideList = document.querySelector('.guides');
+
+	let innerHtml = '';
+
+	docs.forEach((doc) => {
+		const guide = doc.data();
+
+		const li = `
+      <li>
+        <div class="collapsible-header grey lighten-4">${guide.title}</div>
+        <div class="collapsible-body white">${guide.content}</div>
+      </li>
+    `;
+
+		innerHtml += li;
+	});
+
+	guideList.innerHTML = innerHtml;
+};
+
+// get data
+
+// const firebase = require('firebase');
+// const db = firebase.firestore();
+
+db.collection('guides').get().then((snapshot) => {
+	renderGuides(snapshot.docs);
+});
