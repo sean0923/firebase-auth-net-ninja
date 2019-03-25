3bca0bc713edbae185bb18d7a6730a1309099b0f
Sean Hong <iamseanhong@gmail.com>
2019-03-25 17:22:25 -0600

    Implement firebase functions: addAdminRole

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,23 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+exports.addAdminRole = functions.https.onCall((data, context) => {
+	// get user and add custom clain (admin)
+
+	return admin
+		.auth()
+		.getUserByEmail(data.email)
+		.then((user) => {
+			return admin.auth().setCustomUserClaims((user.uid, { admin: true }));
+		})
+		.then(() => {
+			return { message: `Sucess! ${data.email} has been mdde an admin` };
+		})
+		.catch((error) => {
+			return error;
+		});
+});
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
